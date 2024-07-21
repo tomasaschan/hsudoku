5c207b827029aca7ef73011f351e38f8d771c47b
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2024-07-21 23:06:45 +0200

    Implement components as views over the puzzle

--- src/Puzzle.hs
+++ src/Puzzle.hs
@@ -13,3 +13,15 @@ fromList = M.fromList . fmap (\(r, c, v) -> ((r, c), v))
 
 at :: Int -> Int -> Puzzle -> Cell
 at r c p = p M.! (r, c)
+
+type Component = [Item]
+
+-- | Returns the components (row, column and subgrid) that contain the cell at (r, c)
+components :: Int -> Int -> Puzzle -> [Component]
+components r c p =
+  [ [(r, c', at r c' p) | c' <- [0 .. 8]],
+    [(r', c, at r' c p) | r' <- [0 .. 8]],
+    [(r', c', at r' c' p) | r' <- sub r, c' <- sub c]
+  ]
+  where
+    sub x = [x' .. x' + 2] where x' = 3 * (x `div` 3)
--- test/PuzzleSpec.hs
+++ test/PuzzleSpec.hs
@@ -1,38 +1,123 @@
 module PuzzleSpec where
 
 import Data.List (intercalate)
+import Puzzle
 import Puzzle.Parse
 import Puzzle.Print
 import Test.Hspec
 
 spec :: Spec
-spec = describe "puzzle io" $ do
-  it "can read and display the example" $ do
-    let input = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
-    let expected =
-          intercalate
-            "\n"
-            [ "┏━┯━┯━┳━┯━┯━┳━┯━┯━┓",
-              "┃ │ │4┃3│ │ ┃2│ │9┃",
-              "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
-              "┃ │ │5┃ │ │9┃ │ │1┃",
-              "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
-              "┃ │7│ ┃ │6│ ┃ │4│3┃",
-              "┣━┿━┿━╋━┿━┿━╋━┿━┿━┫",
-              "┃ │ │6┃ │ │2┃ │8│7┃",
-              "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
-              "┃1│9│ ┃ │ │7┃4│ │ ┃",
-              "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
-              "┃ │5│ ┃ │8│3┃ │ │ ┃",
-              "┣━┿━┿━╋━┿━┿━╋━┿━┿━┫",
-              "┃6│ │ ┃ │ │ ┃1│ │5┃",
-              "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
-              "┃ │ │3┃5│ │8┃6│9│ ┃",
-              "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
-              "┃ │4│2┃9│1│ ┃3│ │ ┃",
-              "┗━┷━┷━┻━┷━┷━┻━┷━┷━┛"
-            ]
-            <> "\n"
+spec = describe "puzzle" $ do
+  describe "io" $ do
+    it "can read and display the example" $ do
+      let input = "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+      let expected =
+            intercalate
+              "\n"
+              [ "┏━┯━┯━┳━┯━┯━┳━┯━┯━┓",
+                "┃ │ │4┃3│ │ ┃2│ │9┃",
+                "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
+                "┃ │ │5┃ │ │9┃ │ │1┃",
+                "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
+                "┃ │7│ ┃ │6│ ┃ │4│3┃",
+                "┣━┿━┿━╋━┿━┿━╋━┿━┿━┫",
+                "┃ │ │6┃ │ │2┃ │8│7┃",
+                "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
+                "┃1│9│ ┃ │ │7┃4│ │ ┃",
+                "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
+                "┃ │5│ ┃ │8│3┃ │ │ ┃",
+                "┣━┿━┿━╋━┿━┿━╋━┿━┿━┫",
+                "┃6│ │ ┃ │ │ ┃1│ │5┃",
+                "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
+                "┃ │ │3┃5│ │8┃6│9│ ┃",
+                "┠─┼─┼─╂─┼─┼─╂─┼─┼─┨",
+                "┃ │4│2┃9│1│ ┃3│ │ ┃",
+                "┗━┷━┷━┻━┷━┷━┻━┷━┷━┛"
+              ]
+              <> "\n"
 
-    let actual = pretty . cells $ input
-    actual `shouldBe` expected
+      let actual = pretty . cells $ input
+      actual `shouldBe` expected
+
+  describe "components" $ do
+    let puzzle = cells "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+
+    it "returns components in the top left correctly" $ do
+      let actual = components 0 0 puzzle
+
+      actual
+        `shouldBe` [
+                     -- row 0
+                     [ (0, 0, Candidates [1 .. 9]),
+                       (0, 1, Candidates [1 .. 9]),
+                       (0, 2, Solved 4),
+                       (0, 3, Solved 3),
+                       (0, 4, Candidates [1 .. 9]),
+                       (0, 5, Candidates [1 .. 9]),
+                       (0, 6, Solved 2),
+                       (0, 7, Candidates [1 .. 9]),
+                       (0, 8, Solved 9)
+                     ],
+                     -- col 0
+                     [ (0, 0, Candidates [1 .. 9]),
+                       (1, 0, Candidates [1 .. 9]),
+                       (2, 0, Candidates [1 .. 9]),
+                       (3, 0, Candidates [1 .. 9]),
+                       (4, 0, Solved 1),
+                       (5, 0, Candidates [1 .. 9]),
+                       (6, 0, Solved 6),
+                       (7, 0, Candidates [1 .. 9]),
+                       (8, 0, Candidates [1 .. 9])
+                     ],
+                     -- subgrid 0
+                     [ (0, 0, Candidates [1 .. 9]),
+                       (0, 1, Candidates [1 .. 9]),
+                       (0, 2, Solved 4),
+                       (1, 0, Candidates [1 .. 9]),
+                       (1, 1, Candidates [1 .. 9]),
+                       (1, 2, Solved 5),
+                       (2, 0, Candidates [1 .. 9]),
+                       (2, 1, Solved 7),
+                       (2, 2, Candidates [1 .. 9])
+                     ]
+                   ]
+
+    it "returns components in the mid right correctly" $ do
+      let actual = components 4 8 puzzle
+
+      actual
+        `shouldBe` [
+                     -- row 4
+                     [ (4, 0, Solved 1),
+                       (4, 1, Solved 9),
+                       (4, 2, Candidates [1 .. 9]),
+                       (4, 3, Candidates [1 .. 9]),
+                       (4, 4, Candidates [1 .. 9]),
+                       (4, 5, Solved 7),
+                       (4, 6, Solved 4),
+                       (4, 7, Candidates [1 .. 9]),
+                       (4, 8, Candidates [1 .. 9])
+                     ],
+                     -- col 8
+                     [ (0, 8, Solved 9),
+                       (1, 8, Solved 1),
+                       (2, 8, Solved 3),
+                       (3, 8, Solved 7),
+                       (4, 8, Candidates [1 .. 9]),
+                       (5, 8, Candidates [1 .. 9]),
+                       (6, 8, Solved 5),
+                       (7, 8, Candidates [1 .. 9]),
+                       (8, 8, Candidates [1 .. 9])
+                     ],
+                     -- subgrid 8
+                     [ (3, 6, Candidates [1 .. 9]),
+                       (3, 7, Solved 8),
+                       (3, 8, Solved 7),
+                       (4, 6, Solved 4),
+                       (4, 7, Candidates [1 .. 9]),
+                       (4, 8, Candidates [1 .. 9]),
+                       (5, 6, Candidates [1 .. 9]),
+                       (5, 7, Candidates [1 .. 9]),
+                       (5, 8, Candidates [1 .. 9])
+                     ]
+                   ]
