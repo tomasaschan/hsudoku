93b4864948b20f7b4b2af9e2f512eae995a77b89
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2024-07-23 01:42:22 +0200

    Implement 'naked pairs' technique

---
+++ src/Solver/NakedPairs.hs
@@ -0,0 +1,28 @@
+module Solver.NakedPairs where
+
+import Data.Maybe
+
+import Puzzle
+import Solver
+
+nakedPairs :: Technique
+nakedPairs p = listToMaybe pairs >>= toEdit
+  where
+    toEdit (_,_,[],_) = Nothing
+    toEdit (r,c,v:_,_) = Just (r,c,v)
+
+
+    pairs =
+      map (\(r,c,cs) -> (r, c, cs, buddies r c cs))
+        . filter isPair
+          . fmap (\(r,c) -> (r,c, candidates r c p))
+            $ unsolved p
+
+    buddies r c cs =
+      filter (not . null)
+        . fmap (filter (hasCandidates cs) . filter (`elem` unsolved p))
+          $ components r c
+
+    hasCandidates cs (r',c') = cs == candidates r' c' p
+
+    isPair (_,_,cs) = length cs == 2
---
+++ test/Solver/NakedPairsSpec.hs
@@ -0,0 +1,35 @@
+module Solver.NakedPairsSpec where
+
+import Data.Maybe
+
+import Puzzle
+import Solver.NakedPairs
+
+import Test.Hspec
+
+spec :: Spec
+spec = describe "naked pairs technique" $ do
+  let p = puzzle "985724613476531289001689547164297835758316492009845761597168324012453970043972150"
+
+  it "finds a naked pair and constructs a valid edit from it" $ do
+    let e = nakedPairs p
+
+    let allowed =
+          [
+            (2,0,2),
+            (2,0,3),
+            (3,0,2),
+            (3,0,3),
+            (5,0,2),
+            (5,0,3),
+            (7,0,6),
+            (7,0,8),
+            (8,0,6),
+            (8,0,8),
+            (7,8,6),
+            (7,8,8),
+            (8,8,6),
+            (8,8,8)
+          ]
+
+    allowed `shouldContain` [fromMaybe (0,0,0) e]
